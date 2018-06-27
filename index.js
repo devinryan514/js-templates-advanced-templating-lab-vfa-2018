d8b116147b0ffce46289bf44156bdf98a9be0401
Devin Ryan <devin.ryan514@gmail.com>
2018-06-27 03:44:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+function createRecipe(){
+  //render information and send to recipe template
+}
+
+function displayEditForm(){
+  //Fix this
+}
+
+Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+
+Handlebars.registerHelper('displayIngredient', function() {
+  return new Handlebars.safeString(this.body);
+});
+  
+
 function init() {
-  //put any page initialization/handlebars initialization here
+  var formTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML);
+  var formHTML = formTemplate();
+  document.getElementsByTagName('main')[0].innerHTML += formHTML;
+  
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
